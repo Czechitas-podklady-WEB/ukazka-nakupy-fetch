94207d0acbf5d1c243d39c67a1402b4eb639f69e
Martin Podloucký <podlouckymartin@gmail.com>
2023-10-13 17:28:37 +0200

    Prvotní kód aplikace

---
+++ src/pages/index.jsx
@@ -0,0 +1,35 @@
+import { render } from '@czechitas/render';
+import { ShopItem } from '../components/ShopItem';
+import '../global.css';
+import './index.css';
+
+const response = await fetch('https://nakupy.kodim.app/api/sampleweek/mon');
+const data = await response.json();
+const list = data.result.items;
+
+const HomePage = () => (
+  <>
+    <div className="banner"></div>
+    <div className="container">
+      <form className="newitem-form">
+        <label htmlFor="input-name">Položka</label>
+        <input id="input-name" type="text" />
+        <label htmlFor="input-amount">Množství</label>
+        <input id="input-amount" type="text" />
+        <button className="btn-add">Přidat</button>
+      </form>
+      <div className="shoplist">
+        {list.map((item) => (
+          <ShopItem 
+            key={item.id}
+            name={item.product}
+            amount={item.amount + ' ' + item.unit}
+            bought={item.done}
+          />
+        ))}
+      </div>
+    </div>
+  </>
+);
+
+document.querySelector('#root').innerHTML = render(<HomePage />);
